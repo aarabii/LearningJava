d85230002be12f07960922097d2a2abbe5a83118
losier <arv95464@gmail.com>
2022-03-02 18:10:41 +0530

    day2

--- Day2/GCD.java
+++ Day2/GCD.java
@@ -1,5 +1,24 @@
 package Day2;
 
-public class GCD {
+import java.util.Scanner;
 
+public class GCD {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Whats the 1st number: ");
+        int num1 = scanner.nextInt();
+        System.out.println("Whats the 2nd number: ");
+        int num2 = scanner.nextInt();
+
+        int gcd = 1;
+
+        for (int i = 1; i <= num1 && i <= num2; i++){
+
+            if (num1 % i == 0 && num2 % i == 0)
+                gcd = i;
+        }
+
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: "+ gcd);
+    }
 }
